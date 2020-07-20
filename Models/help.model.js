849b0239a5692249f767673ab5c8d7c1fca0454d
Ayan Murad <ayan.murad@itsolution24x7.net>
2020-07-20 10:16:03 -0700

    Feat: Help Content add and get api

--- Models/help.model.js
+++ Models/help.model.js
@@ -14,7 +14,33 @@ const helpSchema = new mongoose.Schema({
     createdDate:{ type:Date, default:Date.now },
 });
 
+const helpContentSchema = new mongoose.Schema({
+  title:{
+    type:String
+  },
+  subTitle:{
+    type:String
+  },
+  image:{
+      type:String
+  },
+  video: {
+      type:String
+  },
+  description:{
+      type:String
+  },
+  cardID:{
+    type:String
+},
+type:{
+  type:String
+},
+  createdDate:{ type:Date, default:Date.now },
+});
+
+const HelpContent = mongoose.model('helpContent', helpContentSchema);
 const HelpData = mongoose.model('help', helpSchema);
 
-
+exports.HelpContent = HelpContent;
 exports.HelpData = HelpData;
