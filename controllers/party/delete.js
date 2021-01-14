24e1df57a8cb0001c9775c0eb1bc9db2666f8b94
fatima-itsol-2019 <fatima.zahid@itsolution24x7.com>
2021-01-14 19:37:48 +0500

    delete party API

--- controllers/party/delete.js
+++ controllers/party/delete.js
@@ -2,13 +2,14 @@
 //***** Modules goes here *****//
 const express = require('express');
 const { PartyData } = require('../../Models/party.model');
-
+const { UserData } = require('../../Models/user.model');
+const auth = require('../../middleware/auth');
 
 const app = express();
 
-app.delete('/', async (req, res) => {
+app.delete('/', auth, async (req, res) => {
 
-    if(req.query.userId == null || req.query.partyId == null){
+    if (req.query.partyId == null) {
         var err = {
             success: false,
             msg: 'Please enter valid User AND Party ID!'
@@ -17,39 +18,59 @@ app.delete('/', async (req, res) => {
         return;
     }
 
-    try {
-
-        const alreadyDeleted = await PartyData.findOne({
-            "members.userId": req.query.userId,
-            "members.isOwner": true,
-            _id: req.query.partyId
-        });
-
-        if(alreadyDeleted == null){
+    await deleteParty(req)
+        .then(() => {
+            var success = {
+                success: true,
+                msg: 'Party deleted successfully!'
+            };
+            res.send(success);
+            return;
+        })
+        .catch((ex) => {
             var err = {
                 success: false,
-                msg: 'This party does not exist'
+                msg: 'There was an error!',
+                data: ex
             };
             res.status(500).send(err);
             return;
-        }
-
-        await PartyData.findOneAndDelete({
-            "members.userId": req.query.userId,
-            "members.isOwner": true,
-            _id: req.query.partyId
         });
 
-        var success = {
-            success: true,
-            msg: 'Party deleted successfully!'
-        };
-        res.send(success);
-        return;
+});
 
-    } catch (err) {
-        return { success: false, error: err, data: null };
-    }
-})
+
+async function deleteParty(req) {
+    return new Promise(async (resolve, reject) => {
+        const user = await UserData.findOne({ _id: req.user._id });
+
+        const party = await PartyData.findOne({
+            _id: req.query.partyId,
+            "members.phone": user.mobile,
+            "members.isOwner": true
+        });
+
+        if (party == null) {
+            var err = {
+                msg: 'Party doesnt exist OR you do not have permission to delete it!'
+            };
+            reject(err);
+        }
+
+        party.members.map(async (pty) => {
+
+            if (pty.phone === user.mobile) {
+                await PartyData.findOneAndDelete({
+                    _id: req.query.partyId
+                }, async (err, result) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(result);
+                });
+            }
+        })
+    })
+}
 
 module.exports = app;
