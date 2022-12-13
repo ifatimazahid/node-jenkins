da4edcea3bf7f1abfe0a51a007f4656bd95bbcd6
Fatima Zahid <ifatimazahid18@gmail.com>
2022-12-13 15:17:08 +0500

    Updations

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,5 +1,5 @@
 echo "Inside deliver file"
-npm run build
+npm run build-sample
 npm start 
 echo "Build and start commands executed successfully"
 echo "Visit http://localhost:3000/api/courses/get in Postman to run your API"
